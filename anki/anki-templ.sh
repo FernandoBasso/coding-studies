c4f3bc73bf9d1519d7c11506e6a57a17c02329aa
Fernando Basso <fernandobasso.br@gmail.com>
2025-04-27 09:38:09 -0300

    anki: Add script to copy template

---
+++ anki/anki-templ.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+##
+# Copy an Anki card's template to the clipboard.
+#
+# All the templates are in ./anki/card-templates/. Copy a template
+# with a command line like this one:
+#
+#   $ ./anki-templ.sh front
+#
+# The template passed as the argument is the name of the file
+# without the extension.
+##
+
+err_no_templ_param=1
+err_no_templ_found=2
+
+if [[ $# == 0 ]]
+then
+  printf '\nProvide the name of the template as the first parameter.\n'
+  exit $err_no_templ_param
+fi
+
+templ="./anki/${1}.html"
+
+if [[ ! -r $templ ]]
+then
+  printf '\nTemplate ‘%s’ not found.\n' "$templ"
+  exit $err_no_templ_found
+else
+  ##
+  # Copy the contents of the template into the clipboard.
+  #
+  0< "$templ" xclip -in -selection clipboard
+
+  printf '\nTemplate ‘%s’ successfully copied to the clipboard.\n' "$templ"
+fi
