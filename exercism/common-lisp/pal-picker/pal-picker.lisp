80cbbcabeea50e22b714bf20427155d4b2767794
Fernando Basso <fernandobasso.br@gmail.com>
2025-05-15 09:11:49 -0300

    exercism(common-lisp): Upadte pal picker doc-comments

--- exercism/common-lisp/pal-picker/pal-picker.lisp
+++ exercism/common-lisp/pal-picker/pal-picker.lisp
@@ -5,6 +5,11 @@
 
 (in-package :pal-picker)
 
+;;;;
+;; pal-picker :: Symbol -> String
+;;
+;; Returns the type of pet based on the personality.
+;;
 (defun pal-picker (personality)
   (cond
     ((string= personality :lazy) "Cat")
@@ -14,6 +19,11 @@
     ((string= personality :talkative) "Bird")
     (t "I don't know... A dragon?")))
 
+;;;;
+;; habitat-filter :: Integer -> Symbol
+;;
+;; Returns the habitat size based on the weight of the animal.
+;;
 (defun habitat-fitter (weight)
   (cond
     ((>= weight 40) :massive)
@@ -22,16 +32,35 @@
     ((and (>= weight 1) (<= weight 9)) :small)
     ((<= weight 0) :just-your-imagination)))
 
+;;;;
+;; feeding-time-p :: Integer -> String
+;;
+;; Returns a string indicating whether the bowl needs refilling based on
+;; the bowl fullness.
+;;
 (defun feeding-time-p (fullness)
   (if (> fullness 20)
     "All is well."
     "It's feeding time!"))
 
+;;;;
+;; pet :: String -> String | NIL
+;;
+;; Returns a string informing we should probably not pet this specific
+;; pet or nil if this kind of pet can be petted.
+;;
 (defun pet (pet)
   (cond
     ((string= pet "Fish") "Maybe not with this pet...")
     (t nil)))
 
+;;;;
+;; play-fetch :: String -> String | NIL
+;;
+;; Returns nil if this kind of pet can play fetch, or a string
+;; indicating we should probably not try to play fetch with this
+;; kind of pet.
+;;
 (defun play-fetch (pet)
   (cond
     ((string= pet "Dog") nil)
