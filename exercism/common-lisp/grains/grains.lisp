c14d64630353cc3c3e8831039db987bad60af73c
Fernando Basso <fernandobasso.br@gmail.com>
2025-05-21 08:34:53 -0300

    exercism(common-lisp): Add grains v1

---
+++ exercism/common-lisp/grains/grains.lisp
@@ -0,0 +1,43 @@
+(defpackage :grains
+  (:use :cl)
+  (:export :square :total))
+(in-package :grains)
+
+(defparameter *board-size* 64)
+
+;;;;
+;; square :: Integer -> Integer
+;;
+;; Returns the amount of grains on a given square.
+;;
+(defun square (n)
+  (labels
+    ((run (i acc)
+       (if (= i n)
+         acc
+         (run
+           (+ i 1)
+           (* acc 2)))))
+    (run 1 1)))
+
+;;;;
+;; total :: Integer
+;;
+;; Returns the total number of grains on a board of *board-size* squares.
+;;
+(defun total ()
+  (labels
+    ((run (i acc sum)
+       (if (= i *board-size*)
+         sum
+         (run
+           (+ i 1)
+           (* acc 2)
+           (+ sum (* acc 2))))))
+    (run 1 1 1)))
+
+;;
+;; References:
+;;
+;; · https://stackoverflow.com/questions/19535023/helper-nested-functions-in-cl
+;;
