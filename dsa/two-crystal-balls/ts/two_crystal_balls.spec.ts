5fe36418e7536470f603a08313632db8fe5ae5f8
Fernando Basso <fernandobasso.br@gmail.com>
2025-07-08 11:37:35 -0300

    dsa(ts): two crystal balls v1

---
+++ dsa/two-crystal-balls/ts/two_crystal_balls.spec.ts
@@ -0,0 +1,21 @@
+import { twoCrystalBalls } from "./two_crystal_balls_v1";
+
+describe("two crystal balls", () => {
+  it("no floor causes the ball to break", function () {
+    // The ball doesn't break from any of these floors.
+    expect(twoCrystalBalls(new Array(8912).fill(false))).toEqual(-1);
+  });
+
+  it("ball breaks, large number of floors", () => {
+    let breakIdx = Math.floor(Math.random() * 15e6);
+    const floors = new Array(15e6).fill(false);
+
+    for (let i = breakIdx; i < 15e6; ++i) {
+      // All floors >= breakIdx are set to true to indicate
+      // the ball breaks of dropped from any of those floors.
+      floors[i] = true;
+    }
+
+    expect(twoCrystalBalls(floors)).toEqual(breakIdx);
+  });
+});
