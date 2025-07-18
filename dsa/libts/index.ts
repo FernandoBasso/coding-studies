656dfcb753f92c8cba3223375059a1a3ee77579b
Fernando Basso <fernandobasso.br@gmail.com>
2025-07-18 10:28:04 -0300

    ts: Add nullable utils

--- dsa/libts/index.ts
+++ dsa/libts/index.ts
@@ -1,4 +1,7 @@
 export const log = console.log.bind(console);
 export const max = Math.max.bind(Math);
 export const min = Math.min.bind(Math);
+
 export { len } from "./len";
+
+export { isNil, isNotNil, type Nullable } from "./nullable";
