2b362e5c093ed421d31e7ee543fca9f590e63d74
Fernando Basso <fernandobasso.br@gmail.com>
2025-07-25 14:37:59 -0300

    exercism(clojure): Armstrong number v1

---
+++ exercism/clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -0,0 +1,32 @@
+;;;
+;;; Check my notes and explanations on the solutions:
+;;;
+;;; • https://fernandobasso.gitlab.io/devnotes/Exercism/
+;;;
+
+(ns armstrong-numbers)
+
+(defn to-digits
+  "to-digits :: Num -> Vector<Num>
+
+   Turns an integer like 123 into a vector of the digits [1 2 3]."
+  [n vod]
+
+  (if (< n 10)
+    (apply vector n vod)
+    (recur
+     (quot n 10)
+     (apply vector (mod n 10) vod))))
+
+(defn armstrong?
+  "armstrong? :: Long -> Bool
+
+   Returns a boolean indicating whether the given number
+   is an Armstrong number."
+  [num]
+  (let [digits (to-digits num [])]
+    (->> digits
+         (map #(.pow (bigdec %) (count digits)))
+         (reduce +)
+         (== num))))
+
