b7ac84bd278c3a66c499a70d4d9f205a5aecd87a
Fernando Basso <fernandobasso.br@gmail.com>
2025-08-06 11:26:25 -0300

    clj: Exercise 3 dec-maker

--- clojure/stuff/src/ch03/exercises.clj
+++ clojure/stuff/src/ch03/exercises.clj
@@ -56,3 +56,23 @@ uniq
 (add100 1)
 ;=> 101
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 3 dec maker
+;;
+(defn dec-maker
+  "Returns a function that decrements the given number by n."
+  [n]
+  (fn [x] (- x n)))
+
+(def dec1 (dec-maker 1))
+(dec1 10)
+;=> 9
+
+(def dec25 (dec-maker 25))
+(dec25 100)
+;=> 75
+
+
+
+
