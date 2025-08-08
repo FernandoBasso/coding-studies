2cbadd9bdd2f5456924e60de431aecee6dcdd380
Fernando Basso <fernandobasso.br@gmail.com>
2025-08-08 10:02:56 -0300

    clj: Add seq examples

---
+++ clojure/stuff/src/ch04/e04.clj
@@ -0,0 +1,41 @@
+(ns ch04.e04)
+
+;;
+;; seq always returns a value that looks and behaves like a list.
+;;
+
+;;;;
+;; List as input.
+;;
+(seq '(1 two III))
+;;=> (1 two III)
+
+(type (seq '(1 two III)))
+;;=> clojure.lang.PersistentList
+
+;;;;
+;; Vector as input.
+;;
+(seq [1 "two" 'III])
+;;=> (1 "two" III)
+
+(type (seq [1 "two" 'III]))
+;;=> clojure.lang.PersistentVector$ChunkedSeq
+
+;;;;
+;; Set as input.
+;;
+(seq #{1 "two" 'III})
+;;=> (III 1 "two")
+
+(type (seq #{1 "two" 'III}))
+;;=> clojure.lang.APersistentMap$KeySeq
+
+(seq {:one 1 :two 'II})
+;;=> ([:one 1] [:two II])
+;;
+;; Note the output is a pair or tuple-like thing. So when a map is
+;; turned into a seq, it becomes a list of pair, or a tuple-like
+;; data structure (which is just a vector of two elements).
+;;;;
+
