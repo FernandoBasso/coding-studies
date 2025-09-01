232276288ba4d415223869c46e3d8028fd8d46ea
Fernando Basso <fernandobasso.br@gmail.com>
2025-09-01 08:25:46 -0300

    dsa(ts): Add stack impl

---
+++ dsa/stack-int/ts/stack.spec.ts
@@ -0,0 +1,82 @@
+import { Stack } from "./stack";
+
+describe("Stack", () => {
+  it("can construct and empty stack", () => {
+    const stack = new Stack<number>();
+
+    expect(stack.size()).toBe(0);
+  });
+
+  describe("push()", () => {
+    it("push to an empty stack", () => {
+      const stack = new Stack<number>();
+      stack.push(40);
+
+      expect(stack.size()).toBe(1);
+    });
+
+    it("push to a non-empty stack", () => {
+      const stack = new Stack<number>();
+      stack.push(40);
+      stack.push(90);
+      stack.push(111);
+
+      expect(stack.size()).toBe(3);
+    });
+  });
+
+  describe("pop()", () => {
+    it("pop from an empty stack", () => {
+      const stack = new Stack<number>();
+
+      expect(stack.pop()).toBe(null);
+    });
+
+    it("pop from a non-empty stack", () => {
+      const stack = new Stack<number>();
+      stack.push(40);
+      stack.push(90);
+
+      const topVal: number | null = stack.pop();
+
+      expect(topVal).toBe(90);
+      expect(stack.size()).toBe(1);
+    });
+
+    it("pop the stack empty", () => {
+      const stack = new Stack<number>();
+      stack.push(40);
+      stack.push(90);
+      stack.push(110);
+
+      expect(stack.pop()).toBe(110);
+      expect(stack.size()).toBe(2);
+
+      expect(stack.pop()).toBe(90);
+      expect(stack.size()).toBe(1);
+
+      expect(stack.pop()).toBe(40);
+      expect(stack.size()).toBe(0);
+
+      expect(stack.pop()).toBe(null);
+      expect(stack.size()).toBe(0);
+    });
+  });
+
+  describe("peek()", () => {
+    it("peek an empty stack", () => {
+      const stack = new Stack<number>();
+
+      expect(stack.peek()).toBe(null);
+    });
+
+    it("peek a non-empty stack", () => {
+      const stack = new Stack<number>();
+
+      stack.push(40);
+      stack.push(90);
+
+      expect(stack.peek()).toBe(90);
+    });
+  });
+});
---
+++ dsa/stack-int/ts/stack.ts
@@ -0,0 +1,61 @@
+type StackNode<T> = {
+  value: T | null;
+  prev: StackNode<T> | null
+};
+
+export class Stack<T> {
+  private _top: StackNode<T> | null;
+  private _size: number;
+
+  constructor() {
+    this._top = null;
+    this._size = 0;
+  }
+
+  /**
+   * Inserts a new value at the top of the stack.
+   */
+  push(val: T): void {
+    const node: StackNode<T> = { value: val, prev: null };
+
+    this._size++;
+
+    if (this._top === null) {
+      this._top = node;
+      return;
+    }
+
+    node.prev = this._top;
+    this._top = node;
+  }
+
+  /**
+   * Returns the value at the top of the stack and removes
+   * it from the stack.
+   */
+  pop(): T | null {
+    if (this._top === null)
+      return null;
+
+    const top = this._top;
+
+    this._size--;
+    this._top = this._top.prev;
+
+    return top.value;
+  }
+
+  /**
+   * Returns the top value without deleting it from the stack.
+   */
+  peek(): T | null {
+    if (this._top === null)
+      return null;
+
+    return this._top.value;
+  }
+
+  size(): number {
+    return this._size;
+  }
+}
