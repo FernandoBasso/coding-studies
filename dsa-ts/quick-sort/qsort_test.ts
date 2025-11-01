942e1d63085cb8a3453ed1b13c14f0a1301d454c
Fernando Basso <fernandobasso.br@gmail.com>
2025-11-01 13:18:34 -0300

    dsa(ts): Add quick sort tests

--- dsa-ts/quick-sort/qsort_test.ts
+++ dsa-ts/quick-sort/qsort_test.ts
@@ -14,5 +14,21 @@ Deno.test("Quick Soft", async (t) => {
     assertEquals(arr, [99]);
   });
 
-  await t.step("")
+  await t.step("multiple-element array", () => {
+    const arr1: Array<number> = [1, 5];
+    qsort(arr1);
+    assertEquals(arr1, [1, 5]);
+
+    const arr2: Array<number> = [5, 1];
+    qsort(arr2);
+    assertEquals(arr2, [1, 5]);
+
+    const arr3: Array<number> = [-42, 1, 9, 9, 9];
+    qsort(arr3);
+    assertEquals(arr3, [-42, 1, 9, 9, 9]);
+
+    const arr4: Array<number> = [9, 9, 9, -42, 1];
+    qsort(arr4);
+    assertEquals(arr4, [-42, 1, 9, 9, 9]);
+  });
 });
