e973097bea0151e80e4b8e4eda1c247074c9bbc3
Fernando Basso <fernandobasso.br@gmail.com>
2025-11-13 20:42:09 -0300

    dsa(js): Implement merge for merge sort

--- dsa-js/merge_sort/msort.js
+++ dsa-js/merge_sort/msort.js
@@ -1,8 +1,30 @@
 var log = console.log.bind(console);
 
-export function merge() {
-  return [];
+export function merge(xs, ys) {
+  var i = 0,
+      j = 0,
+      xsLen = xs.length,
+      ysLen = ys.length
+      res = [];
+
+  while (i < xsLen && j < ysLen)
+    if (xs[i] < ys[j])
+      res.push(xs[i++]);
+    else
+      res.push(ys[j++]);
+
+    if (i === xsLen)
+      res.push(...ys.slice(j));
+
+    if (j === ysLen)
+      res.push(...ys.slice(i));
+
+  return res;
 }
 
 export function msort() {
+}
+
+if (import.meta.main) {
+  merge([1], [2]);
 }
--- dsa-js/merge_sort/msort.spec.js
+++ dsa-js/merge_sort/msort.spec.js
@@ -8,11 +8,32 @@ describe("Merge Sorted Arrays", () => {
     expect(res).toEqual([]);
   });
 
-  test("single element & empty", () => {
+  test("single element arrays", () => {
     var xs = [1];
-    var ys = [];
+    var ys = [2];
     var res = merge(xs, ys);
-    expect(res).toEqual([1]);
+    expect(res).toEqual([1, 2]);
+  });
+
+  test("multiple elements, same length", () => {
+    var xs = [1, 3, 5];
+    var ys = [2, 4, 6];
+    var res = merge(xs, ys);
+    expect(res).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test("first array smaller than second array", () => {
+    var xs = [5];
+    var ys = [3, 4, 6];
+    var res = merge(xs, ys);
+    expect(res).toEqual([3, 4, 5, 6]);
+  });
+
+  test("first array larger than second array", () => {
+    var xs = [1, 3, 4];
+    var ys = [2, 5];
+    var res = merge(xs, ys);
+    expect(res).toEqual([1, 2, 3, 4, 5]);
   });
 });
 
