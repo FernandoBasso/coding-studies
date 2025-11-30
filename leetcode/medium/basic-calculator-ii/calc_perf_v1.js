6d0a9ab9e6de9bd84848509075c11fc2b49f33ef
Fernando Basso <fernandobasso.br@gmail.com>
2025-11-30 09:41:35 -0300

    leetcode(js): Basic Calc II examples

--- leetcode/medium/basic-calculator-ii/calc_perf_v1.js
+++ leetcode/medium/basic-calculator-ii/calc_perf_v1.js
@@ -4,42 +4,56 @@ function isDigit(c) {
   return /\d/.test(c);
 }
 
+function toInt(digits) {
+  return Number.parseInt(digits.join(""), 10);
+}
+
 export function calc(str) {
   const operands = [];
   let operator = "+";
 
   for (let i = 0; i < str.length; ++i) {
-    if (str[i] === " ") {
+    if (str[i] === " ")
       continue;
-    }
 
     if (isDigit(str[i])) {
       const digits = [];
-      while (i < str.length && isDigit(str[i])) {
+      while (i < str.length && isDigit(str[i]))
         digits.push(str[i++]);
-      }
+
       --i;
 
-      let num = Number.parseInt(digits.join(""), 10);
+      let num = toInt(digits);
 
-      if (operator === "-") {
+      if (operator === "-")
         num = -1 * num;
-      } else if (operator === "*") {
+      else if (operator === "*")
         num = operands.pop() * num;
-      } else if (operator === "/") {
+      else if (operator === "/")
         num = operands.pop() / num | 0;
-      }
 
       operands.push(num);
-    } else {
+    } else
       operator = str[i];
-    }
   }
 
   return operands.reduce((acc, x) => acc + x, 0);
 }
 
 if (import.meta.main) {
-  const res1 = calc("3 + 5 / 2");
+  const res1 = calc("2 +3*5 * 1 * 1 * 1 - 1 + 1");
   log({ res1 });
+  //=> 17
+
+  const res2 = calc("3/ 2   ");
+  log({ res2 });
+  //=> 1
+
+  const res3 = calc("3 +5 /2");
+  log({ res3 });
+  //=> 5
+
+  const res4 = calc("234 * 1 * 2 / 2 + 1 - 1");
+  log({ res4 });
+  //=> 234
 }
