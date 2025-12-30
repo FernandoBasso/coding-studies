c89d2f07ac7540c529eb72479a3f0933a3de2b2f
Fernando Basso <fernandobasso.br@gmail.com>
2025-12-30 17:32:53 -0300

    exercism(js): Windowing System v2

--- exercism/javascript/windowing-system/windowing-system.js
+++ exercism/javascript/windowing-system/windowing-system.js
@@ -1,7 +1,7 @@
 // @ts-check
 
-// eslint-disable-next-line no-unused-vars
-const log = console.log.bind(console);
+const max = Math.max.bind(Math);
+const min = Math.min.bind(Math);
 
 function Size(width = 80, height = 60) {
   this.width = width;
@@ -15,7 +15,7 @@ function Size(width = 80, height = 60) {
 Size.prototype.resize = function resize(width, height) {
   this.width = width;
   this.height = height;
-}
+};
 
 function Position(x = 0, y = 0) {
   this.x = x;
@@ -29,85 +29,37 @@ function Position(x = 0, y = 0) {
 Position.prototype.move = function move(x, y) {
   this.x = x;
   this.y = y;
-}
+};
 
 class ProgramWindow {
-  screenSize = new Size(800, 600);
-  size = new Size();
-  position = new Position();
-
-  /**
-   * @param {number} width
-   * @returns {number}
-   */
-  #width(width) {
-    if (width < 1)
-      return 1;
-
-    const remainingWidth = this.screenSize.width - this.position.x;
-
-    return Math.min(remainingWidth, width);
-  }
-
-  /**
-   * @param {number} height
-   * @returns {number}
-   */
-  #height(height) {
-    if (height < 1)
-      return 1;
-
-    const remainingHeight = this.screenSize.height - this.position.y;
-
-    return Math.min(remainingHeight, height);
+  constructor() {
+    this.screenSize = new Size(800, 600);
+    this.size = new Size();
+    this.position = new Position();
   }
 
   /**
    * @param {Size} newSize
    */
   resize(newSize) {
-    const width = this.#width(newSize.width);
-    const height = this.#height(newSize.height);
+    const maxW = this.screenSize.width - this.position.x;
+    const maxH = this.screenSize.height - this.position.y;
 
-    this.size.resize(width, height);
+    let newW = max(1, min(newSize.width, maxW));
+    let newH = max(1, min(newSize.height, maxH));
+
+    this.size.resize(newW, newH);
   }
 
   /**
-   * @param {number} newX
+   * @param {Position} newPos
    */
-  #x(newX) {
-    if (newX < 0)
-      return 0;
+  move(newPos) {
+    const maxX = this.screenSize.width - this.size.width;
+    const maxY = this.screenSize.height - this.size.height;
 
-    const newWinXEnd = this.position.x + this.size.width + newX;
-
-    if (newWinXEnd > this.screenSize.width)
-      return newX - (newWinXEnd - this.screenSize.width);
-
-    return newX;
-  }
-
-  /**
-   * @param {number} newY
-   */
-  #y(newY) {
-    if (newY < 0)
-      return 0;
-
-    const newWinYEnd = this.position.y + this.size.height + newY;
-
-    if (newWinYEnd > this.screenSize.height)
-      return newY - (newWinYEnd - this.screenSize.height);
-
-    return newY;
-  }
-
-  /**
-   * @param {Position} newPosition
-   */
-  move(newPosition) {
-    const newX = this.#x(newPosition.x);
-    const newY = this.#y(newPosition.y);
+    const newX = max(0, min(maxX, newPos.x));
+    const newY = max(0, min(maxY, newPos.y));
 
     this.position.move(newX, newY);
   }
