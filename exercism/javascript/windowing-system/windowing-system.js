360b1a4b3e183aa2316ba54692fc8494776d6a81
Fernando Basso <fernandobasso.br@gmail.com>
2025-12-31 10:50:56 -0300

    exercism(js): Windowing System v3

--- exercism/javascript/windowing-system/windowing-system.js
+++ exercism/javascript/windowing-system/windowing-system.js
@@ -1,8 +1,16 @@
 // @ts-check
 
+//
+// https://fernandobasso.gitlab.io/devnotes/Exercism/JavaScript/Windowing-System-Exercism-Solution-in-JavaScript
+//
+
 const max = Math.max.bind(Math);
 const min = Math.min.bind(Math);
 
+/**
+ * @param {number} height
+ * @param {number} width
+ */
 function Size(width = 80, height = 60) {
   this.width = width;
   this.height = height;
@@ -15,8 +23,12 @@ function Size(width = 80, height = 60) {
 Size.prototype.resize = function resize(width, height) {
   this.width = width;
   this.height = height;
-};
+}
 
+/**
+ * @param {number} x
+ * @param {number} y
+ */
 function Position(x = 0, y = 0) {
   this.x = x;
   this.y = y;
@@ -29,7 +41,7 @@ function Position(x = 0, y = 0) {
 Position.prototype.move = function move(x, y) {
   this.x = x;
   this.y = y;
-};
+}
 
 class ProgramWindow {
   constructor() {
@@ -41,25 +53,25 @@ class ProgramWindow {
   /**
    * @param {Size} newSize
    */
-  resize(newSize) {
-    const maxW = this.screenSize.width - this.position.x;
-    const maxH = this.screenSize.height - this.position.y;
+  resize({ width: requestedWidth, height: requestedHeight }) {
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
 
-    let newW = max(1, min(newSize.width, maxW));
-    let newH = max(1, min(newSize.height, maxH));
+    const newWidth = max(1, min(maxWidth, requestedWidth));
+    const newHeight = max(1, min(maxHeight, requestedHeight));
 
-    this.size.resize(newW, newH);
+    this.size.resize(newWidth, newHeight);
   }
 
   /**
-   * @param {Position} newPos
+   * @param {Position} pos
    */
-  move(newPos) {
+  move({ x: requestedX, y: requestedY }) {
     const maxX = this.screenSize.width - this.size.width;
     const maxY = this.screenSize.height - this.size.height;
 
-    const newX = max(0, min(maxX, newPos.x));
-    const newY = max(0, min(maxY, newPos.y));
+    const newX = max(0, min(maxX, requestedX));
+    const newY = max(0, min(maxY, requestedY));
 
     this.position.move(newX, newY);
   }
