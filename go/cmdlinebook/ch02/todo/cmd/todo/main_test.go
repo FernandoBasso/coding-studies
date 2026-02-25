6d4420aa253d9b3b735476427786bb13828c6fbc
Fernando Basso <fernandobasso.br@gmail.com>
2026-02-25 09:04:04 -0300

    go(cmdline): Test main

--- go/cmdlinebook/ch02/todo/cmd/todo/main_test.go
+++ go/cmdlinebook/ch02/todo/cmd/todo/main_test.go
@@ -1,11 +1,79 @@
 package todo_test
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
 
 	"devhowto.dev/gocmdlinebook/ch02/todo/cmd/todo"
 )
+
+var (
+	binName  = "todo"
+	fileName = ".todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests...")
+
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+
+	os.Remove(binName)
+	os.Remove(fileName)
+
+	os.Exit(result)
+}
+
+func TestTestTodoCLI(t *testing.T) {
+	task := "Task #1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("can add a task", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("can list tasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		want := task + "\n"
+
+		if string(out) != want {
+			t.Errorf("Want %q, got %q", want, string(out))
+		}
+	})
+}
 
 func TestTodo(t *testing.T) {
 	t.Run("can add a task", func(t *testing.T) {
--- go/cmdlinebook/ch02/todo/main.go
+++ go/cmdlinebook/ch02/todo/main.go
@@ -1,11 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"devhowto.dev/gocmdlinebook/ch02/todo/cmd/todo"
+)
+
+const todoFileName = ".todos.json"
 
 func main() {
-	xs := []int{10, 20, 30, 40, 50, 60, 70}
-	xs = append(xs[:2], xs[3:]...)
-	fmt.Printf("%#v\n", xs)
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		// Concat all args with a space.
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
 }
 
 // go run ./main.go
