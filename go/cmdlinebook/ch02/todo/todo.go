69de43a9c1aa10c7aebd34c0c88abe2a04795ed3
Fernando Basso <fernandobasso.br@gmail.com>
2026-05-18 08:42:41 -0300

    go(cmdline): Add FindByID()

--- go/cmdlinebook/ch02/todo/todo.go
+++ go/cmdlinebook/ch02/todo/todo.go
@@ -3,6 +3,7 @@ package todo
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -40,3 +41,24 @@ func (t *Todo) Add(title string) (task, error) {
 
 	return task, nil
 }
+
+// FindByID returns a task found by ID, or a zero-valued task ID not found.
+func (t *Todo) FindByID(id uuid.UUID) task {
+	for _, item := range t.Tasks {
+		fmt.Printf("\nID: %d\n", item.ID)
+		if item.ID == id {
+			return item
+		}
+	}
+
+	return task{}
+}
+
+// func (t *Todo) Complete(id int) (task, error) {
+// 	taskToComplete := t.FindByID(id)
+// 	if taskToComplete.CreatedAt.IsZero() {
+// 		return task{}, errors.New(fmt.Sprint("could not find task with id %d", id))
+// 	}
+//
+// 	t.Tasks[id].CompletedAt = time.Now()
+// }
--- go/cmdlinebook/ch02/todo/todo_test.go
+++ go/cmdlinebook/ch02/todo/todo_test.go
@@ -29,7 +29,43 @@ func TestTodo(t *testing.T) {
 			}
 		})
 
+		t.Run("FindByID())", func(t *testing.T) {
+			t.Run("returns a zero-valued task when not found", func(t *testing.T) {
+				todo := todo.Todo{}
+				todo.Add("Learn DDD")
+				todo.Add("Learn TDD")
+
+				// Let's assume we'll not have an ID of 9001 while
+				// the tests are running.
+				lastTask := todo.FindByID(uuid.New())
+				if !lastTask.IsZero() {
+					wantQ(t, "an empty task", "a valid, filled-in task")
+				}
+			})
+
+			t.Run("returns a valid, filled in task when found by ID", func(t *testing.T) {
+				todo := todo.Todo{}
+				taskJustAdded, _ := todo.Add("Play Tomb Raider I")
+
+				taskFoundByID := todo.FindByID(taskJustAdded.ID)
+				if taskFoundByID.Title != "Play Tomb Raider I" {
+					wantQ(t, "the task just added", "an empty, zero-valued task")
+				}
+			})
+		})
 	})
+
+	// t.Run("Complete())", func(t *testing.T) {
+	// 	t.Run("Complete task", func(t *testing.T) {
+	// 		todo := todo.Todo{}
+	// 		todo.Add("Learn TDD")
+	//
+	// 		task, _ := todo.Complete(1)
+	// 		if task.CompletedAt.IsZero() {
+	// 			wantQ(t, "true", "false")
+	// 		}
+	// 	})
+	// })
 }
 
 func wantQ(t *testing.T, want, got string) {
