8844d31b4c9f3c84989807ba93f039532f7c6f80
Fernando Basso <fernandobasso.br@gmail.com>
2026-05-26 06:55:24 -0300

    go(cmdline): Add missing method comments

--- go/cmdlinebook/ch02/todo/todo.go
+++ go/cmdlinebook/ch02/todo/todo.go
@@ -16,21 +16,26 @@ type task struct {
 	CompletedAt time.Time
 }
 
-// IzZero returns a boolean indicating whether the task an empty struct.
-func (t *task) IsZero() bool {
-	return t.Title == "" && t.CreatedAt.IsZero() && t.CompletedAt.IsZero()
-}
-
 type todo struct {
 	Tasks map[uuid.UUID]task
 }
 
+// New returns a new todo with a slice of tasks with an initial
+// capacity of 32.
 func New() *todo {
 	return &todo{
 		Tasks: make(map[uuid.UUID]task, 32),
 	}
 }
 
+// IzZero returns a boolean indicating whether the task an empty struct.
+func (t *task) IsZero() bool {
+	return t.Title == "" && t.CreatedAt.IsZero() && t.CompletedAt.IsZero()
+}
+
+// Add adds a new task to the collection and returns the task and a nil
+// error, or, returns an empty task and an error the given task title is
+// empty.
 func (t *todo) Add(title string) (task, error) {
 	if title == "" {
 		return task{}, errors.New("task title cannot be empty")
@@ -59,6 +64,10 @@ func (t *todo) FindByID(id uuid.UUID) task {
 	return task{}
 }
 
+// Complete marks the task with the given ID as complete (sets its
+// CompletedAt with time.Now()), and returns the completed task and a
+// nil error. Returns a zero-valued task and an error if the task with
+// the given id cannot be found.
 func (t *todo) Complete(id uuid.UUID) (task, error) {
 	taskToComplete := t.FindByID(id)
 	if taskToComplete.IsZero() {
@@ -68,18 +77,21 @@ func (t *todo) Complete(id uuid.UUID) (task, error) {
 
 	item := t.Tasks[id]
 	item.CompletedAt = time.Now()
-
 	t.Tasks[id] = item
 
 	return item, nil
 }
 
-//
-// Maps in Go are hash tables that rehash and relocate entries when they
-// grow. If &t.Tasks[id] were a real pointer, that pointer could be
-// invalidated the next time anyone inserted into the map and you’d have a
-// dangling reference to memory that now holds a different key’s value, or
-// nothing at all. Rather than introduce some pinning mechanism or pretend
-// the pointer is stable when it isn’t, Go just says: map elements aren’t
-// addressable
-//
+// Delete deletes the task with the given id, and returns the deleted
+// task and a nil error. If the task with the given id is not found,
+// returns a zero-valued task and an error.
+func (t *todo) Delete(id uuid.UUID) (task, error) {
+	taskToDelete, ok := t.Tasks[id]
+	if !ok {
+		return task{}, fmt.Errorf("could not find task with id %s", id)
+	}
+
+	delete(t.Tasks, id)
+
+	return taskToDelete, nil
+}
